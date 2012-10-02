0952e1b5fcf5ad6fe787d4ae42c58eb21825864c
gabegabe <gabe.winter@hotmail.com>
2012-10-02 11:13:51 +0700

    sesi1

---
+++ praktikum/src/sesi01/Person.java
@@ -0,0 +1,19 @@
+//Nama	: Satari AHmad Tahir
+//Nim	: 4508210070
+//Praktikum Pemrograman 1
+
+public class Person{
+	public static void main(String[] args){
+		String nim = "4508210070";
+		String nama;
+		nama = "Satari Ahmad Tahir";
+		
+		System.out.println("Nama saya adalah: " + nama);
+		System.out.println("Nim : " + nim);
+	}
+}
+
+//public class Person merupakan suatu class yang bisa diakses oleh class lainnya
+//public static void main(String[] args) maksudnya adalah metode utamanya
+//String nim = "4508210070"; variable nim dengan tipe data string
+//
